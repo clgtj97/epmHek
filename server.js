9d73a9467cc742b71c730ae2d0e6b78446edcb9d
clgtj91 <clgjt97@gmail.com>
2023-12-15 17:49:21 -0800

    netlify.toml update-1

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const stripe = require("stripe")('sk_test_51OEdyLFrT7IGAV5fcgwarKO2gavlnX3HCINtU
 app.use(express.static("public"));
 app.use(express.json());
 
-// Add CORS headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 app.post("/create-payment-intent", async (req, res) => {
   const { amount, currency, items } = req.body;
